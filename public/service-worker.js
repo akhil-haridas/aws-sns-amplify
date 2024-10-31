58fe700283c9d54ce3d3168cb00b2c164c63fd6a
akhil-haridas <4khilharidas@gmail.com>
2024-10-31 11:39:32 +0530

    updated service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,13 +1,21 @@
-self.addEventListener("push", function (event) {
+// self.addEventListener("push", function (event) {
+//     const data = event.data.json();
+//     const options = {
+//         body: data.body,
+//         icon: "/icon.png",
+//         vibrate: [100, 50, 100],
+//         data: {
+//             dateOfArrival: Date.now(),
+//             primaryKey: "2",
+//         },
+//     };
+//     event.waitUntil(self.registration.showNotification(data.title, options));
+// });
+
+self.addEventListener('push', (event) => {
     const data = event.data.json();
-    const options = {
+    self.registration.showNotification(data.title, {
         body: data.body,
-        icon: "/icon.png",
-        vibrate: [100, 50, 100],
-        data: {
-            dateOfArrival: Date.now(),
-            primaryKey: "2",
-        },
-    };
-    event.waitUntil(self.registration.showNotification(data.title, options));
+        icon: '/icon.png',
+    });
 });
