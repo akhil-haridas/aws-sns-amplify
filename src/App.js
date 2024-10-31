248de3aa1888705b96ea50ff337522ec4cce2eed
akhil-haridas <4khilharidas@gmail.com>
2024-10-31 10:58:10 +0530

    cleanup app

--- src/App.js
+++ src/App.js
@@ -1,32 +1,10 @@
-import React, { useEffect } from "react";
-import { Amplify } from "aws-amplify";
-import awsconfig from "./aws-exports";
-import { PushNotification } from "@aws-amplify/pushnotification";
-import NotificationService from "./NotificationService"; // Import NotificationService
-
-Amplify.configure(awsconfig);
+import React from "react";
 
 function App() {
-  useEffect(() => {
-    requestPermission();
-    NotificationService.setupNotificationListener();
-  }, []);
-
-  const requestPermission = async () => {
-    const permission = await PushNotification.requestPermissions();
-    if (permission === "granted") {
-      const token = await PushNotification.getToken();
-      console.log("Notification token:", token);
-      // You can send the token to your backend to store it for notifications
-    }
-  };
-
   return (
     <div className="App">
       <h1>AWS Amplify Push Notifications</h1>
-      <button onClick={NotificationService.sendTestNotification}>
-        Send Test Notification
-      </button>
+      <button>Send Test Notification</button>
     </div>
   );
 }
